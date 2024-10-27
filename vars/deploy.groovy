a2f76b1dae842292ab727ed920207500520f11ec
Umamahesh180396 <43702107+Umamahesh180396@users.noreply.github.com>
2024-10-27 15:02:03 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,7 +1,7 @@
 def call(){
   echo "Deploying code"
   echo "Stopping and deleting existing container"
-  sh 'docker compsoe down'
+  sh 'docker compose down'
   sh 'docker compose up -d'
   echo "Code is deployed"
 }
